3305ad4b165b21ffc73f6030a9fa14b3ecb3836f
Flavio Moreni <fmoreni@gmail.com>
2026-06-11 19:33:51 -0300

    Finalizando a integração com banco de dados

---
+++ Fiap.Api.Donation7/Repository/CategoriaRepository.cs
@@ -0,0 +1,52 @@
+﻿using Fiap.Api.Donation7.Data;
+using Fiap.Api.Donation7.Model;
+using Fiap.Api.Donation7.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fiap.Api.Donation7.Repository
+{
+    public class CategoriaRepository : ICategoriaRepository
+    {
+
+        private readonly DataContext _dataContext;
+
+        public CategoriaRepository(DataContext dataContext) { 
+            _dataContext = dataContext;
+        }
+
+
+        public IList<CategoriaModel> FindAll()
+        {
+            return _dataContext.Categorias.AsNoTracking().ToList();
+        }
+
+        public CategoriaModel FindById(int id)
+        {
+            return _dataContext.Categorias.AsNoTracking().FirstOrDefault(c => c.CategoriaId == id);
+        }
+
+        public int Insert(CategoriaModel categoriaModel)
+        {
+            _dataContext.Categorias.Add(categoriaModel);
+            _dataContext.SaveChanges();
+
+            return categoriaModel.CategoriaId;
+        }
+
+        public void Update(CategoriaModel categoriaModel)
+        {
+            _dataContext.Categorias.Update(categoriaModel);
+            _dataContext.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var categoria = new CategoriaModel() { CategoriaId = id };
+
+            _dataContext.Categorias.Remove(categoria);
+            _dataContext.SaveChanges();
+        }
+
+
+    }
+}
---
+++ Fiap.Api.Donation7/Repository/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,16 @@
+﻿using Fiap.Api.Donation7.Model;
+
+namespace Fiap.Api.Donation7.Repository.Interfaces
+{
+    public interface ICategoriaRepository
+    {
+
+        public IList<CategoriaModel> FindAll();
+        public CategoriaModel FindById(int id);
+        public int Insert(CategoriaModel categoriaModel);
+        public void Update(CategoriaModel categoriaModel);
+        public void Delete(int id);
+
+
+    }
+}
